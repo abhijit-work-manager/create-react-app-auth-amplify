3e6e71a5858349ea23f6b56b4047fea7828cfab5
abhijit-work-manager <78414063+abhijit-work-manager@users.noreply.github.com>
2021-02-07 17:04:28 +0530

    test commit

---
+++ src/project/AddProject.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
+class AddProject extends Component {
+  render() {
+    return "this is it"
+  }
+}
+
+export default withAuthenticator(AddProject, true);
